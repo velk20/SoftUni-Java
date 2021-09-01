3923f172bd682d7df34e41b18ad941db99bd05e4
Angel Mladenov <angelmladenov333@gmail.com>
2021-09-01 22:40:28 +0300

    Laptop

--- 2.Java Fundamentals/src/L01BasicSyntaxStatementsLoops/Exercise/T10RageExpenses.java
+++ 2.Java Fundamentals/src/L01BasicSyntaxStatementsLoops/Exercise/T10RageExpenses.java
@@ -5,6 +5,29 @@ import java.util.Scanner;
 public class T10RageExpenses {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int lostCount = Integer.parseInt(scanner.nextLine());
+        double headSetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboradPrice = Double.parseDouble(scanner.nextLine());
+        double displayPrice = Double.parseDouble(scanner.nextLine());
+        double sum = 0;
+        int counterKey = 0;
+        for (int i = 1; i <= lostCount; i++) {
+            if (i % 2 == 0) {
+                sum += headSetPrice;
+            }
+            if (i % 3 == 0) {
+                sum += mousePrice;
+            }
+            if (i % 2 == 0 && i % 3 == 0) {
+                sum += keyboradPrice;
+                counterKey++;
+                if (counterKey % 2 == 0) {
+                    sum += displayPrice;
+                }
+            }
+        }
 
+        System.out.printf("Rage expenses: %.2f lv.",sum);
     }
 }
--- 2.Java Fundamentals/src/L01BasicSyntaxStatementsLoops/MoreExercise/T02EnglishNameOfTheLastDigit.java
+++ 2.Java Fundamentals/src/L01BasicSyntaxStatementsLoops/MoreExercise/T02EnglishNameOfTheLastDigit.java
@@ -5,6 +5,53 @@ import java.util.Scanner;
 public class T02EnglishNameOfTheLastDigit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int len = Integer.parseInt(scanner.nextLine());;
+        DigitGetter(len);
+    }
+
+    static void DigitGetter(int l) {
+
+
+           int n = (l % 10);
+
+
+        switch (n) {
+            case 0:
+                System.out.printf("zero");
+                break;
+
+            case 1:
+                System.out.printf("one");
+                break;
+            case 2:
+                System.out.printf("two");
+                break;
+            case 3:
+                System.out.printf("three");
+                break;
+            case 4:
+                System.out.printf("four");
+                break;
+            case 5:
+                System.out.printf("five");
+                break;
+            case 6:
+                System.out.printf("six");
+                break;
+            case 7:
+                System.out.printf("seven");
+                break;
+            case 8 :
+                System.out.printf("eight");
+                break;
+            case 9:
+                System.out.printf("nine");
+                break;
+            case 10:
+                System.out.printf("ten");
+                break;
+
+        }
 
     }
 }
