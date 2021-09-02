00fb631b08322d38c7f55b18beebdd439e0a9b16
Angel Mladenov <angelmladenov333@gmail.com>
2021-09-02 14:52:29 +0300

    Laptop-SecondTasksAdeed Datatypes....

--- 2.Java Fundamentals/src/L02DataTypesandVariables/Exercise/T01IntegerOperations.java
+++ 2.Java Fundamentals/src/L02DataTypesandVariables/Exercise/T01IntegerOperations.java
@@ -1,10 +1,26 @@
 package L02DataTypesandVariables.Exercise;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class T01IntegerOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+        BigInteger c = scanner.nextBigInteger();
+        BigInteger d = scanner.nextBigInteger();
+
+        BigInteger sum ;
+        sum = a.add(b);
+
+        BigInteger dev;
+        dev = sum.divide(c);
+
+        BigInteger as;
+        as = dev.multiply(d);
+
+        System.out.println(as);
 
     }
 }
--- 2.Java Fundamentals/src/L02DataTypesandVariables/Exercise/T02SumDigits.java
+++ 2.Java Fundamentals/src/L02DataTypesandVariables/Exercise/T02SumDigits.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class T02SumDigits {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        int sum = 0;
+        for (int i = 0; i < a.length(); i++) {
+            char digit = a.charAt(i);
+            int dig = Character.getNumericValue(digit);
+            sum += dig;
+
+        }
+        System.out.printf("%d",sum);
 
     }
 }
