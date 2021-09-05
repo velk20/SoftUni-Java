53133cd8fc0047263446ff3b34c13635b323d5cc
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2021-09-05 14:57:06 +0300

    Fundamentals start

--- 2.Java Fundamentals/src/L01BasicSyntaxStatementsLoops/MoreExercise/T05Messages.java
+++ 2.Java Fundamentals/src/L01BasicSyntaxStatementsLoops/MoreExercise/T05Messages.java
@@ -5,6 +5,30 @@ import java.util.Scanner;
 public class T05Messages {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        String output = "";
+        for (int i = 0; i < n; i++) {
+            String num = scanner.nextLine();
+            int len = num.length();
 
+                char dig = num.charAt(0);
+                int mainDigit = Character.getNumericValue(dig);
+            int offset = 0;
+            if (mainDigit == 8 || mainDigit == 9) {
+                 offset = ((mainDigit - 2) * 3) + 1;
+
+            } else {
+
+                 offset = (mainDigit - 2) * 3;
+            }
+
+            int index = (offset + len - 1);
+            if (mainDigit == 0) {
+                output += " ";
+            } else {
+                output += (char) (index + 97);
+            }
+        }
+        System.out.println(output);
     }
 }
--- 2.Java Fundamentals/src/L02DataTypesandVariables/Exercise/T09SpiceMustFlow.java
+++ 2.Java Fundamentals/src/L02DataTypesandVariables/Exercise/T09SpiceMustFlow.java
@@ -1,10 +1,32 @@
 package L02DataTypesandVariables.Exercise;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class T09SpiceMustFlow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int yeld = Integer.parseInt(scanner.nextLine());
+        int collect  = 0;
+        int days = 0;
+        if (yeld >= 100)
+        {
+            while (yeld >= 100)
+            {
+                days++;
+                collect += yeld;
+                yeld -= 10;
+
+            }
+            collect -= (days + 1) * 26;
+            System.out.println(days);
+            System.out.println(collect);
+        }
+        else
+        {
+            System.out.println(days);
+            System.out.println(collect);
+        }
 
     }
 }
