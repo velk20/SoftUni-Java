8eddb47239793161a0ffb468b0de24c6200b1ad5
Angel Mladenov <angelmladenov333@gmail.com>
2021-11-02 22:32:00 +0200

    Laptop-SecondTasksAdeed Datatypes....

--- 2.Java Fundamentals/src/L07MapsLambdaAndStreamAPI/Exercise/T03LegendaryFarming.java
+++ 2.Java Fundamentals/src/L07MapsLambdaAndStreamAPI/Exercise/T03LegendaryFarming.java
@@ -10,55 +10,37 @@ public class T03LegendaryFarming {
 •	Valanyr - requires 250 Fragments
 •	Dragonwrath - requires 250 Motes
 */
-        //TODO
-        Map<String, Integer> map = new TreeMap<>();
+
+        Map<String, Integer> legendary = new TreeMap<>();
+        Map<Integer, String> trash = new TreeMap<>();
         List<String> input = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
-        boolean finished = false;
-        while (true) {
+
+        while (input.size() > 0) {
             int quantity = Integer.parseInt(input.get(0));
+            String material = input.get(1).toLowerCase();
+            if (material.equals("shards") || material.equals("fragments") || material.equals("motes")) {
+                if (!legendary.containsKey(material)) {
+                    legendary.put(material, quantity);
+                } else {
+                    int currentQuantity = legendary.get(material);
+                    if (currentQuantity + quantity >= 250) {
+                        System.out.printf("%s obtained\n", material);
+                        legendary.put(material, currentQuantity + quantity - 250);
+                        break;
+                    }
 
-            String material = input.get(1);
-
-            if (!map.containsKey(material)) {
-                map.put(material, quantity);
-            }else{
-                map.put(material, map.get(material) + quantity);
+                }
             }
 
-            switch (material) {
-                case "Shadowmourne":
-                    if (map.get(material) >= 250) {
-                        System.out.println("Shadowmourne obtained!");
-                        finished = true;
-                    }
-                    break;
-                case "Valanyr":
-                    if (map.get(material) >= 250) {
-                        System.out.println("Valanyr obtained!");
-                        finished = true;
-                    }
-                    break;
-                    case "Dragonwrath":
-                    if (map.get(material) >= 250) {
-                        System.out.println("Dragonwrath obtained!");
-                        finished = true;
-                    }
-                    break;
-            }
-            if (finished) {
-                break;
-            }
-            input.remove(0);
-            input.remove(0);
+            else {
+                if (!trash.containsKey(quantity)) {
+                    trash.put(quantity, material);
+                } else {
 
-
-            if (input.size() == 0) {
-                break;
+                    //TODO
+                }
             }
         }
 
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.printf("%s: %d\n", entry.getKey(), entry.getValue());
-        }
     }
 }
--- 2.Java Fundamentals/src/L07MapsLambdaAndStreamAPI/Exercise/T04Orders.java
+++ 2.Java Fundamentals/src/L07MapsLambdaAndStreamAPI/Exercise/T04Orders.java
@@ -1,10 +1,45 @@
 package L07MapsLambdaAndStreamAPI.Exercise;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class T04Orders {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Map<String, Map<Double, Integer>> map = new LinkedHashMap<>();
+        String input = scanner.nextLine();
 
+        while (!input.equals("buy")) {
+            String[] product = input.split(" ");
+            String name = product[0];
+            double price = Double.parseDouble(product[1]);
+            int quantity = Integer.parseInt(product[2]);
+            if (!map.containsKey(name)) {
+                Map<Double, Integer> newMap = new LinkedHashMap<>();
+                newMap.put(price, quantity);
+                map.put(name, newMap);
+            } else {
+                Map<Double, Integer> newMap = map.get(name);
+                for (Map.Entry<Double, Integer> entry : newMap.entrySet()) {
+                    if (entry.getKey() != price) {
+                        int oldQuantity = entry.getValue();
+                        newMap.remove(entry.getKey());
+                        newMap.put(price, quantity + oldQuantity);
+                        map.put(name, newMap);
+                    }
+                }
+            }
+
+            input = scanner.nextLine();
+
+        }
+
+        for (Map.Entry<String, Map<Double, Integer>> stringMapEntry : map.entrySet()) {
+            String name = stringMapEntry.getKey();
+            Map<Double, Integer> qwe = stringMapEntry.getValue();
+            for (Map.Entry<Double, Integer> doubleIntegerEntry : qwe.entrySet()) {
+
+                System.out.printf("%s -> %.2f\n", name, doubleIntegerEntry.getKey() * doubleIntegerEntry.getValue());
+            }
+        }
     }
 }
