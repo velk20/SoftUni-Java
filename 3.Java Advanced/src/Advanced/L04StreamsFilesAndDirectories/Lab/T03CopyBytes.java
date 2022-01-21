505849225e51ade83bb145c5c2903dbf0669897e
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-01-21 14:45:00 +0200

    advanced

--- 3.Java Advanced/src/Advanced/L04StreamsFilesAndDirectories/Lab/T03CopyBytes.java
+++ 3.Java Advanced/src/Advanced/L04StreamsFilesAndDirectories/Lab/T03CopyBytes.java
@@ -1,4 +1,36 @@
 package Advanced.L04StreamsFilesAndDirectories.Lab;
 
+import java.io.*;
+import java.util.*;
+
 public class T03CopyBytes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = "D:\\SoftUni-Java\\3.Java Advanced\\src\\Advanced\\L04StreamsFilesAndDirectories\\Lab\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String output = "D:\\SoftUni-Java\\3.Java Advanced\\src\\Advanced\\L04StreamsFilesAndDirectories\\Lab\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\03.CopyBytesOutput.txt";
+
+        try (InputStream inputStream = new FileInputStream(input);
+             OutputStream outputStream = new FileOutputStream(output)) {
+            int oneByte = inputStream.read();
+
+            while (oneByte >= 0) {
+                if (oneByte == 32 || oneByte == 10) {
+                    outputStream.write((char)oneByte);
+                } else {
+                    String digits = String.valueOf(oneByte);
+                    for (int i = 0; i < digits.length(); i++) {
+                        outputStream.write(digits.charAt(i));
+                    }
+                }
+                oneByte = inputStream.read();
+            }
+
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- 3.Java Advanced/src/Advanced/L04StreamsFilesAndDirectories/Lab/T04ExtractIntegers.java
+++ 3.Java Advanced/src/Advanced/L04StreamsFilesAndDirectories/Lab/T04ExtractIntegers.java
@@ -1,4 +1,29 @@
 package Advanced.L04StreamsFilesAndDirectories.Lab;
 
+import java.io.*;
+import java.util.*;
+
 public class T04ExtractIntegers {
+    public static void main(String[] args) throws IOException {
+        String input = "D:\\SoftUni-Java\\3.Java Advanced\\src\\Advanced\\L04StreamsFilesAndDirectories\\Lab\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String output = "D:\\SoftUni-Java\\3.Java Advanced\\src\\Advanced\\L04StreamsFilesAndDirectories\\Lab\\04. " +
+                "Java-Advanced-Files-and-Streams-Lab-Resources\\04.ExtractIntegersOutput.txt";
+
+
+
+        FileInputStream inputStream = new FileInputStream(input);
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream(output));
+        Scanner scanner = new Scanner(inputStream);
+
+        while (scanner.hasNext()) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                printWriter.println(number);
+            }
+            scanner.next();
+        }
+
+        inputStream.close();
+        printWriter.close();
+    }
 }
