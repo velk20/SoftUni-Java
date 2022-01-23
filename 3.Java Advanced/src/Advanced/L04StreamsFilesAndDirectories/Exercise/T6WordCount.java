59698a8fda916ccba3da3230555df5e60f2e805d
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-01-23 18:02:00 +0200

    advanced

--- 3.Java Advanced/src/Advanced/L04StreamsFilesAndDirectories/Exercise/T6WordCount.java
+++ 3.Java Advanced/src/Advanced/L04StreamsFilesAndDirectories/Exercise/T6WordCount.java
@@ -6,9 +6,7 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class T6WordCount {
@@ -18,8 +16,34 @@ public class T6WordCount {
 
         String output = "D:\\SoftUni-Java\\3.Java Advanced\\src\\Advanced\\L04StreamsFilesAndDirectories\\Exercise" +
                 "\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\output.txt";
+        List<String> list = Files.readAllLines(input);
+        Set<String> set = new HashSet<>();
+        Map<String, Integer> map = new HashMap<>();
 
-        Set<String> lines = new HashSet<>(Files.readAllLines(input));
+        for (String s : list) {
+            String[] strings = s.split(" ");
+            set.addAll(Arrays.asList(strings));
+        }
 
+        String[] text = Files.readString(input1).split("\\s+");
+        for (String s : text) {
+            if (set.contains(s)) {
+                if (!map.containsKey(s)) {
+                    map.put(s, 1);
+                } else {
+                    map.put(s, map.get(s) + 1);
+                }
+            }
+        }
+
+        map.entrySet().stream().sorted(
+                (leftElement,rightElement) ->{
+                    return rightElement.getValue().compareTo(leftElement.getValue());
+                }
+        ).forEach(
+                e->{
+                    System.out.printf("%s - %d\n",e.getKey(),e.getValue());
+                }
+        );
     }
 }
