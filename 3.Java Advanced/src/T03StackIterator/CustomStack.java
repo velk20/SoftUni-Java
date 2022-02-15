5c4b576d24b66e7ded52ec0308392d57d8959633
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-02-15 21:58:47 +0200

    12

---
+++ 3.Java Advanced/src/T03StackIterator/CustomStack.java
@@ -0,0 +1,53 @@
+package T03StackIterator;
+
+import java.util.NoSuchElementException;
+
+public class CustomStack {
+    private int[] data;
+    private static final int INITIAL_CAPACITY = 4;
+    private int size;
+    private int capacity;
+
+    public CustomStack() {
+        this.data = new int[INITIAL_CAPACITY];
+        this.size = 0;
+        this.capacity = INITIAL_CAPACITY;
+    }
+
+    public void push(int element) {
+        this.data[size] = element;
+        size++;
+        grow();
+    }
+
+    public int pop() {
+        checkEmpty();
+        int removedItem = this.data[size - 1];
+        this.data[size - 1] = 0;
+        size--;
+        return removedItem;
+    }
+    public int peek() {
+        checkEmpty();
+        return this.data[this.size - 1];
+    }
+
+    private void checkEmpty(){
+        if (this.size == 0) {
+            throw new NoSuchElementException("CustomStack is empty");
+        }
+    }
+
+    public void grow() {
+        if (this.size == this.capacity) {
+            this.capacity = this.capacity * 2;
+            int[] temp = new int[this.capacity];
+            for (int i = 0; i < this.size; i++) {
+                temp[i] = this.data[i];
+            }
+            this.data = temp;
+        }
+
+
+    }
+}
