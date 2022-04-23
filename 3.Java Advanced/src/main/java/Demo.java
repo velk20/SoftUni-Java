f63b3e5496071e409c979459a7f345fcc8dce383
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-04-23 20:56:41 +0300

    1

--- 3.Java Advanced/src/main/java/Demo.java
+++ 3.Java Advanced/src/main/java/Demo.java
@@ -1,15 +1,58 @@
-import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public  class Demo {
     public static void main(String[] args) {
-        int count = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        while (count < 10) {
 
-            System.out.println("Welcome!");
+        System.out.println("Please enter the three sides of a triangle");
+        System.out.print("a = ");
+        int a = Integer.parseInt(scanner.nextLine());
+        System.out.print("b = ");
+        int b = Integer.parseInt(scanner.nextLine());
+        System.out.print("c = ");
+        int c = Integer.parseInt(scanner.nextLine());
 
-            count++;
+        if (!checkForValidTriangle(a, b, c)) {
+            System.out.println("This triangle is NOT valid");
+            return;
+        }
+
+        if (checkForEquilateralTriangle(a, b, c)) {
+            System.out.println("The triangle is Equilateral");
+
+        } else if (checkForIsoscelesTriangle(a, b, c)) {
+            System.out.println("The triangle is Isosceles");
+
+        } else System.out.println("The triangle is Scalene");
+
+        if (checkForRectangularTriangle(a, b, c)) {
+            System.out.println("Also the triangle is Rectangular");
 
         }
+
+
+
     }
+
+    public static boolean checkForRectangularTriangle(int a, int b, int c) {
+        return (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) ||
+                (Math.pow(b, 2) + Math.pow(c, 2) == Math.pow(a, 2)) ||
+                (Math.pow(c, 2) + Math.pow(a, 2) == Math.pow(b, 2));
+    }
+
+
+    public static boolean checkForEquilateralTriangle(int a, int b, int c) {
+        return a == b && a == c;
+    }
+
+    public static boolean checkForIsoscelesTriangle(int a, int b, int c) {
+        return (a == b) || (b == c) || (c == a);
+    }
+    public static boolean checkForValidTriangle(int a, int b, int c) {
+        return a + b > c &&
+                b + c > a &&
+                c + a > b;
+    }
+
 }
